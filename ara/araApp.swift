1d8ff703142a5f2dfc565db2a9814f5795271058
Suprabath <suprabathkondapally@gmail.com>
2026-07-20 21:52:50 +0530

    chore: initialize project

---
+++ ara/araApp.swift
@@ -0,0 +1,17 @@
+//
+//  araApp.swift
+//  ara
+//
+//  Created by Suprabath Kondapally on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct araApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
